a1851a3c13c8cd0415628b5978b90d1ff6825058
Karen Arispe <karispe@ucol.mx>
2024-09-26 13:52:29 -0600

    Landing page

--- resources/views/Dashboard/Admin/index.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('Dashboard.master')
-
-@section('titulo')
-    @if (session()->has('nombre'))
-        <h1>Bienvenido {{ session('nombre') }}</h1>
-        <h2>Rol: {{ session('rol') }}</h2>
-    @endif
-@endsection
