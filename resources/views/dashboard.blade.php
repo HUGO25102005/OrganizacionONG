2f8cd1ab76959481108d51699a61f8cc67f8bae1
Ernesto <ejimenez26@ucol.mx>
2024-10-11 10:56:11 -0600

    agregue pagina terminos y condiciones

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,7 +6,7 @@
     @endif
 
     <x-slot name="header">
-        <h2 class="text-xl text-gray-800 leading-tight">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
