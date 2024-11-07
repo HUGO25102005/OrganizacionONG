8aabf3e76995277fa8f7b40311d9e8842a87684f
Manuel Valencia <mvalencia18@ucol.mx>
2024-11-06 22:05:09 -0600

    Nada

--- resources/views/layouts/sections/nav_coordinador.blade.php
+++ resources/views/layouts/sections/nav_coordinador.blade.php
@@ -1,5 +1,5 @@
 <div class="shrink-0 flex items-center">
-    <a href="{{ route('admin.home') }}">
+    <a href="{{ route('coordinador.home') }}">
         <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
     </a>
 </div>
@@ -8,8 +8,8 @@
     <x-nav-link :href="route('coordinador.home')" :active="request()->routeIs('coordinador.home')">
         {{ __('Home') }}
     </x-nav-link>
-
 </div>
+
 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
             <x-nav-link :href="route('coordinador.panelControl')" :active="request()->routeIs('coordinador.panelControl')">
                 {{ __('Panel de Control') }}
@@ -17,13 +17,13 @@
 </div>
 
 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-            <x-nav-link :href="route('coordinador.beneficiarios')" :active="request()->routeIs('coordinador.beneficiarios')">
-                {{ __('Beneficiarios') }}
-            </x-nav-link>
+    <x-nav-link :href="route('coordinador.programas')" :active="request()->routeIs('coordinador.programas')">
+        {{ __('Programas') }}
+    </x-nav-link>
 </div>
 
 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-            <x-nav-link :href="route('coordinador.programas')" :active="request()->routeIs('coordinador.programas')">
-                {{ __('Programas') }}
+            <x-nav-link :href="route('coordinador.beneficiarios')" :active="request()->routeIs('coordinador.beneficiarios')">
+                {{ __('Beneficiarios') }}
             </x-nav-link>
 </div>
