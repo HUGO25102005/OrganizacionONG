ef0dd67587ad7575f0a30402c399a2f1292dbaf3
Ernesto <ejimenez26@ucol.mx>
2024-12-02 14:31:30 -0600

    formularios de colabora

--- resources/views/Page/colabora.blade.php
+++ resources/views/Page/colabora.blade.php
@@ -1,6 +1,6 @@
 @extends('Page.layouts.app')
 @section('importaciones')
-    @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/css/colabora.css'])
+    @vite(['resources/css/app.css', 'resources/css/colabora.css'])
 @endsection
 @section('content')
 
@@ -326,112 +326,117 @@
             });
         });
     </script>
+
 @endsection
-<div id="coordinatorModal" class="fixed inset-0 flex items-center justify-center z-50 bg-gray-900 bg-opacity-50 hidden">
+
+{{-- <div id="coordinatorModal" class="fixed inset-0 flex items-center justify-center z-50 bg-gray-900 bg-opacity-50 hidden">
     <div class="bg-white rounded-lg p-6 w-full max-w-lg shadow-xl" style="width: 900px; height: 700px;">
         <h2 class="text-xl font-bold mb-4 text-[#3B3636]">Solicitud a Coordinador</h2>
+
+        <form id="coordinadorForm" action="{{ route('coordinador.store') }}" method="POST">
+            @csrf
         <!-- Formulario de entrada -->
         <div class="grid grid-cols-2 gap-4">
             <!-- Nombre completo -->
             <div class="relative mb-4">
                 <i class='bx bx-user absolute left-2 top-2.5 text-[#3B3636]'></i>
-                <input type="text" placeholder="Nombre completo"
+                <input type="text" id="nombrecompleto" placeholder="Nombre completo"
                     class="pl-4 py-2 bg-[#E6ECF8] rounded-full w-full drop-shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 text-[#3B3636] text-xs">
             </div>
             <!-- Habilidades clave -->
             <div class="relative mb-4">
                 <i class='bx bx-brain absolute left-2 top-2.5 text-[#3B3636]'></i>
-                <input type="text" placeholder="Habilidades clave"
+                <input type="text" id="habilidades" placeholder="Habilidades clave"
                     class="pl-4 py-2 bg-[#E6ECF8] rounded-full w-full drop-shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 text-[#3B3636] text-xs">
             </div>
             <!-- Fecha de nacimiento -->
             <div class="relative mb-4">
                 <i class='bx bx-calendar absolute left-2 top-2.5 text-[#3B3636]'></i>
-                <input type="date" placeholder="Fecha de nacimiento"
+                <input type="date" id="fecha_nacimiento" placeholder="Fecha de nacimiento"
                     class="pl-4 py-2 bg-[#E6ECF8] rounded-full w-full drop-shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 text-[#3B3636] text-xs placeholder:text-[#3B3636]">
             </div>
             <!-- Idiomas -->
             <div class="relative mb-4">
                 <i class='bx bx-globe absolute left-2 top-2.5 text-[#3B3636]'></i>
-                <input type="text" placeholder="Idiomas"
+                <input type="text" id="idiomas" placeholder="Idiomas"
                     class="pl-4 py-2 bg-[#E6ECF8] rounded-full w-full drop-shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 text-[#3B3636] text-xs">
             </div>
             <!-- Género -->
             <div class="relative mb-4">
                 <i class='bx bx-male-female absolute left-2 top-2.5 text-[#3B3636]'></i>
-                <input type="text" placeholder="Género"
+                <input type="text" id="genero" placeholder="Género"
                     class="pl-4 py-2 bg-[#E6ECF8] rounded-full w-full drop-shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 text-[#3B3636] text-xs">
             </div>
             <!-- Función clave -->
             <div class="relative mb-4">
                 <i class='bx bx-briefcase-alt absolute left-2 top-2.5 text-[#3B3636]'></i>
-                <input type="text" placeholder="Función clave"
+                <input type="text" id="funcion_clave" placeholder="Función clave"
                     class="pl-4 py-2 bg-[#E6ECF8] rounded-full w-full drop-shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 text-[#3B3636] text-xs">
             </div>
             <!-- Domicilio -->
             <div class="relative mb-4">
                 <i class='bx bx-home absolute left-2 top-2.5 text-[#3B3636]'></i>
-                <input type="text" placeholder="Domicilio"
+                <input type="text" id="domicilio" placeholder="Domicilio"
                     class="pl-4 py-2 bg-[#E6ECF8] rounded-full w-full drop-shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 text-[#3B3636] text-xs">
             </div>
             <!-- Área de supervisión -->
             <div class="relative mb-4">
                 <i class='bx bx-map-pin absolute left-2 top-2.5 text-[#3B3636]'></i>
-                <input type="text" placeholder="Área de supervisión"
+                <input type="text" id="area_supervision" placeholder="Área de supervisión"
                     class="pl-4 py-2 bg-[#E6ECF8] rounded-full w-full drop-shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 text-[#3B3636] text-xs">
             </div>
             <!-- Teléfono -->
             <div class="relative mb-4">
                 <i class='bx bx-phone absolute left-2 top-2.5 text-[#3B3636]'></i>
-                <input type="tel" placeholder="Teléfono"
+                <input type="tel" id="telefono" placeholder="Teléfono"
                     class="pl-4 py-2 bg-[#E6ECF8] rounded-full w-full drop-shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 text-[#3B3636] text-xs">
             </div>
             <!-- Capacidad de manejo de equipos -->
             <div class="relative mb-4">
                 <i class='bx bx-group absolute left-2 top-2.5 text-[#3B3636]'></i>
-                <input type="text" placeholder="Capacidad de manejo de equipos"
+                <input type="text" id="manejo_equipos" placeholder="Capacidad de manejo de equipos"
                     class="pl-4 py-2 bg-[#E6ECF8] rounded-full w-full drop-shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 text-[#3B3636] text-xs">
             </div>
             <!-- Correo electrónico -->
             <div class="relative mb-4">
                 <i class='bx bx-envelope absolute left-2 top-2.5 text-[#3B3636]'></i>
-                <input type="email" placeholder="Correo electrónico"
+                <input type="email" id="correo" placeholder="Correo electrónico"
                     class="pl-4 py-2 bg-[#E6ECF8] rounded-full w-full drop-shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 text-[#3B3636] text-xs">
             </div>
             <!-- Conocimiento de herramientas -->
             <div class="relative mb-4">
                 <i class='bx bx-wrench absolute left-2 top-2.5 text-[#3B3636]'></i>
-                <input type="text" placeholder="Conocimiento de herramientas"
+                <input type="text" id="conocimiento_herramientas" placeholder="Conocimiento de herramientas"
                     class="pl-4 py-2 bg-[#E6ECF8] rounded-full w-full drop-shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 text-[#3B3636] text-xs">
             </div>
             <!-- Especialización en cursos -->
             <div class="relative mb-4">
                 <i class='bx bx-book-open absolute left-2 top-2.5 text-[#3B3636]'></i>
-                <input type="text" placeholder="Especialización en cursos"
+                <input type="text" id="cursos" placeholder="Especialización en cursos"
                     class="pl-4 py-2 bg-[#E6ECF8] rounded-full w-full drop-shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 text-[#3B3636] text-xs">
             </div>
             <!-- Disponibilidad de horario -->
             <div class="relative mb-4">
                 <i class='bx bx-time-five absolute left-2 top-2.5 text-[#3B3636]'></i>
-                <input type="text" placeholder="Disponibilidad de horario"
+                <input type="text" id="disponibilidad_horarios" placeholder="Disponibilidad de horario"
                     class="pl-4 py-2 bg-[#E6ECF8] rounded-full w-full drop-shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 text-[#3B3636] text-xs">
             </div>
             <!-- Experiencia laboral -->
             <div class="relative mb-4">
                 <i class='bx bx-history absolute left-2 top-2.5 text-[#3B3636]'></i>
-                <input type="text" placeholder="Experiencia laboral"
+                <input type="text" id="experiencia_laboral" placeholder="Experiencia laboral"
                     class="pl-4 py-2 bg-[#E6ECF8] rounded-full w-full drop-shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 text-[#3B3636] text-xs">
             </div>
             <!-- Disponibilidad de viajes -->
             <div class="relative mb-4">
                 <i class='bx bx-plane absolute left-2 top-2.5 text-[#3B3636]'></i>
-                <input type="text" placeholder="Disponibilidad de viajes"
+                <input type="text" id="disponibilidad_viajes" placeholder="Disponibilidad de viajes"
                     class="pl-4 py-2 bg-[#E6ECF8] rounded-full w-full drop-shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 text-[#3B3636] text-xs">
             </div>
             <!-- Experiencia en sector educativo -->
             <div class="relative mb-4">
                 <i class='bx bx-book-reader absolute left-2 top-2.5 text-[#3B3636]'></i>
-                <input type="text" placeholder="Experiencia en sector educativo"
+                <input type="text" id="experiencia_educativa" placeholder="Experiencia en sector educativo"
                     class="pl-4 py-2 bg-[#E6ECF8] rounded-full w-full drop-shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 text-[#3B3636] text-xs">
             </div>
         </div>
@@ -440,5 +445,6 @@
                 class="bg-blue-100 text-black px-4 py-2 rounded-full mr-2 shadow-inner">Cerrar</button>
             <button class="bg-[#063663] text-white px-4 py-2 rounded-full shadow-md">Enviar Solicitud</button>
         </div>
+        </form>
     </div>
-</div>
+</div> --}}
--- resources/views/Page/layouts/modals/modal_nuevo_coordinador.blade.php
+++ resources/views/Page/layouts/modals/modal_nuevo_coordinador.blade.php
@@ -1,4 +1,5 @@
 <div x-data="{ open: false, tab: 'personal' }">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- Botón para abrir el modal -->
     <button @click="open = true" class="experience-donate">
         <img src="{{ asset('images/unirme.png') }}" alt="Unirme" class="donate-img w-28 h-22">
@@ -33,7 +34,7 @@
             
 
             <!-- Contenido del formulario -->
-            <form action="{{ route('coord.store') }}" method="POST" class="p-4">
+            <form action="{{ route('coord.store') }}"  id="coordinadorForm" method="POST" class="p-4">
                 @csrf
 
                 <div class="relative mt-1 overflow-hidden">
@@ -48,94 +49,89 @@
                             x-transition:leave-end="opacity-0" class="w-full px-4 space-y-4 flex-shrink-0">
                             <div class="grid grid-cols-2 gap-4 p-4 bg-gray-100 rounded-lg">
                                 <div class="mb-4">
-                                    <input type="text" name="name" placeholder="Nombre"
+                                    <input type="text" value="{{ old('name') }}" id="co_name" name="name" placeholder="Nombre"
                                         class="w-full form-control py-2 border border-gray-300 rounded-lg shadow-sm focus:ring-blue-500">
                                 </div>
                                 <div class="mb-4">
-                                    <input type="text" name="apellido_paterno" placeholder="Apellido Paterno"
+                                    <input type="text" value="{{ old('apellido_paterno') }}" id="co_apellido_paterno" name="apellido_paterno" placeholder="Apellido Paterno"
                                         class="w-full form-control py-2 border border-gray-300 rounded-lg shadow-sm focus:ring-blue-500">
                                 </div>
                                 <div class="mb-4">
-                                    <input type="text" name="apellido_materno" placeholder="Apellido Materno"
+                                    <input type="text" value="{{ old('apellido_materno') }}" id="co_apellido_materno" name="apellido_materno" placeholder="Apellido Materno"
                                         class="w-full form-control py-2 border border-gray-300 rounded-lg shadow-sm focus:ring-blue-500">
                                 </div>
                                 <div class="mb-4">
-                                    <input type="email" name="email" placeholder="Correo electrónico"
+                                    <input type="email" value="{{ old('email') }}" id="co_email" name="email" placeholder="Correo electrónico"
                                         class="w-full form-control py-2 border border-gray-300 rounded-lg shadow-sm focus:ring-blue-500">
                                 </div>
                                 <div class="mb-4">
                                     <label for="genero" class="block text-gray-700 font-bold mb-2">Género</label>
-                                    <select name="genero" id="genero"
-                                        class="w-full py-2 px-3 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-                                        required>
-                                        <option value="" disabled selected>Selecciona una opción</option>
-                                        <option value="M">Masculino</option>
-                                        <option value="F">Femenino</option>
+                                    <select name="genero" id="co_genero"
+                                        class="w-full py-2 px-3 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500">
+                                        <option value="" disabled {{ old('genero') == '' ? 'selected' : '' }}>Selecciona una opción</option>
+                                        <option value="M" {{ old('genero') == 'M' ? 'selected' : '' }}>Masculino</option>
+                                        <option value="F" {{ old('genero') == 'F' ? 'selected' : '' }}>Femenino</option>
                                     </select>
                                 </div>
                                 <div class="mb-4">
-                                    <label for="fecha_nacimiento" class="block text-gray-700 font-bold mb-2">Fecha
-                                        Nacimiento</label>
-                                    <input type="date" name="fecha_nacimiento"
+                                    <label for="fecha_nacimiento" class="block text-gray-700 font-bold mb-2">Fecha Nacimiento</label>
+                                    <input type="date" value="{{ old('fecha_nacimiento') }}" id="co_fecha_nacimiento" name="fecha_nacimiento"
                                         class="w-full form-control py-2 border border-gray-300 rounded-lg shadow-sm focus:ring-blue-500">
                                 </div>
-
+                                
                                 <div class="mb-4">
-                                    <input type="tel" name="telefono" placeholder="Teléfono"
+                                    <input type="tel" value="{{ old('telefono') }}" id="co_telefono" name="telefono" placeholder="Teléfono"
                                         class="w-full form-control py-2 border border-gray-300 rounded-lg shadow-sm focus:ring-blue-500">
                                 </div>
                                 <div class="mb-4">
-                                    <input type="text" name="direccion" placeholder="Dirección"
+                                    <input type="text" value="{{ old('direccion') }}" id="co_direccion" name="direccion" placeholder="Dirección"
                                         class="w-full form-control py-2 border border-gray-300 rounded-lg shadow-sm focus:ring-blue-500">
                                 </div>
+                                
                                 <!-- País -->
                                 <div class="mb-4">
-                                    <input type="text" name="pais" placeholder="País"
+                                    <input type="text" value="{{ old('pais') }}" id="co_pais" name="pais" placeholder="País"
                                         class="w-full py-2 px-3 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-                                        required maxlength="100">
+                                        maxlength="100">
                                 </div>
-
+                                
                                 <!-- Estado -->
                                 <div class="mb-4">
-                                    <input type="text" name="estado" placeholder="Estado"
+                                    <input type="text" value="{{ old('estado') }}" id="co_estado" name="estado" placeholder="Estado"
                                         class="w-full py-2 px-3 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-                                        required maxlength="100">
+                                        maxlength="100">
                                 </div>
-
+                                
                                 <!-- Municipio -->
                                 <div class="mb-4">
-                                    <input type="text" name="municipio" placeholder="Municipio"
+                                    <input type="text" value="{{ old('municipio') }}" id="co_municipio" name="municipio" placeholder="Municipio"
                                         class="w-full py-2 px-3 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-                                        required maxlength="100">
+                                        maxlength="100">
                                 </div>
-
+                                
                                 <!-- Código Postal -->
                                 <div class="mb-4">
-                                    <input type="text" name="cp" placeholder="Código Postal"
+                                    <input type="text" value="{{ old('cp') }}" id="co_cp" name="cp" placeholder="Código Postal"
                                         class="w-full py-2 px-3 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-                                        required minlength="5">
+                                        minlength="5">
                                 </div>
-
-                                {{--    Hora inicio  --}}
-
+                                
+                                {{-- Hora inicio --}}
                                 <div class="mb-4">
-                                    <label for="hora_inicio" class="block text-gray-700 font-medium mb-2">Hora de
-                                        inicio</label>
-                                    <input type="time" id="hora_inicio" name="hora_inicio"
-                                        class="w-full py-2 px-3 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-                                        required>
+                                    <label for="hora_inicio" class="block text-gray-700 font-medium mb-2">Hora de inicio</label>
+                                    <input type="time" value="{{ old('hora_inicio') }}" id="co_hora_inicio" name="hora_inicio"
+                                        class="w-full py-2 px-3 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500">
                                 </div>
-
-                                {{-- hora fin --}}
+                                
+                                {{-- Hora fin --}}
                                 <div class="mb-4">
-                                    <label for="hora_fin" class="block text-gray-700 font-medium mb-2">Hora de
-                                        inicio</label>
-                                    <input type="time" id="hora_fin" name="hora_fin"
-                                        class="w-full py-2 px-3 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-                                        required>
+                                    <label for="hora_fin" class="block text-gray-700 font-medium mb-2">Hora de fin</label>
+                                    <input type="time" value="{{ old('hora_fin') }}" id="co_hora_fin" name="hora_fin"
+                                        class="w-full py-2 px-3 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500">
                                 </div>
-                                <input type="hidden" id="password" name="password" value="12345678">
-                                <input type="hidden" id="password_confirmation" name="password_confirmation" value="12345678">
+                                <input type="hidden" value="{{ old('password') }}" id="password" name="password">
+                                <input type="hidden" value="{{ old('password_confirmation') }}" id="password_confirmation" name="password_confirmation">
+                                
                             </div>
                         </div>
                     </div>
@@ -143,7 +139,7 @@
 
                 <!-- Botones -->
                 <div class="flex justify-between mt-6">
-                    <button type="submit"
+                    <button onclick="coordinatorForm()"
                         class="bg-blue-600 text-white rounded-lg px-6 py-2 hover:bg-blue-500">Enviar</button>
                     <button type="button" @click="open = false"
                         class="text-red-600 hover:underline">Cancelar</button>
@@ -152,3 +148,17 @@
         </div>
     </div>
 </div>
+
+@vite(['resources/js/coordinatorform.js'])
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('coordinadorForm');
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault(); // Evita el envío del formulario
+        console.log('El formulario ha sido prevenido de ser enviado.');
+        // Aquí puedes agregar lógica adicional, como validaciones o alertas
+    });
+});
+
+</script>
